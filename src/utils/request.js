15bdc8c957b957afe3326013015ec460aeb83f96
张少漫 <zhangsm@jinggon.com>
2022-06-28 16:36:36 +0800

    配件总排期添加筛选功能

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,10 +7,11 @@ let base_url;
 
 //http://192.168.1.166:9998 --EK测试地址
 //http://192.168.1.166      --EK生产地址
+//https://aps.eklights.cn      --EK生产地址
 //http://192.168.18.82:9998 --易事特测试地址
 //http://192.168.18.82      --易事特地址
 //http://121.9.64.70:9999   --恒美外网访问地址
-let apsUrl = 'http://192.168.1.166';  //isgo 外网 http://3nr9908182.zicp.vip  qf http://a493486951.wocp.fun
+let apsUrl = 'https://aps.eklights.cn';  //isgo 外网 http://3nr9908182.zicp.vip  qf http://a493486951.wocp.fun
 
 localStorage.setItem('apsurl', apsUrl);
 let _this = this;
