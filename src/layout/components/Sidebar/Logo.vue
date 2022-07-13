898f59a0ce1616ee9a2b3a495ae300afa2b88dc0
张少漫 <zhangsm@jinggon.com>
2022-07-13 15:43:24 +0800

    修改logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       title: "APS",
-      logo: require("../../../icons/svg/logo.png"),
+      logo: require("../../../icons/svg/logo.jpg"),
       //   "https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png",
     };
   },
