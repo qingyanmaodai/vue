7b66a96ced386f3a9e89a23105b5ac76dc811259
张少漫 <zhangsm@jinggon.com>
2022-07-13 15:30:12 +0800

    美特

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,8 +11,9 @@ let base_url;
 //http://192.168.18.82:9998 --易事特测试地址
 //http://192.168.18.82      --易事特地址
 //http://121.9.64.70:9998   --恒美外网访问地址
+//http://192.168.30.23:9999 --美特
 
-let apsUrl = 'http://192.168.1.166';  //isgo 外网 http://3nr9908182.zicp.vip  qf http://a493486951.wocp.fun
+let apsUrl = 'http://192.168.30.23:9999';  //isgo 外网 http://3nr9908182.zicp.vip  qf http://a493486951.wocp.fun
 
 
 localStorage.setItem('apsurl', apsUrl);
