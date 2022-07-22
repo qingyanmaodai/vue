7542078534e5834fb786e9cd133c99e331d0054e
张少漫 <zhangsm@jinggon.com>
2022-07-22 09:19:53 +0800

    易事特甘特图

--- src/views/PageReportProduction/gantt-schedule/resource-gantt-chart-index.vue
+++ src/views/PageReportProduction/gantt-schedule/resource-gantt-chart-index.vue
@@ -58,10 +58,7 @@
         </div>
       </div>
     </div>
-    
 </template>
-<script src="https://unpkg.com/@popperjs/core@2"></script>
-    <script src="https://unpkg.com/tippy.js@6"></script>
 <script>
 
 import GSTC from "./plugin/gstc.esm.min.js";
