75d28b90f7600824643c08e0b545645be01685b3
张少漫 <zhangsm@jinggon.com>
2022-08-29 19:47:13 +0800

    优化

--- src/views/PageReportProduction/ImportAnalysis/index.vue
+++ src/views/PageReportProduction/ImportAnalysis/index.vue
@@ -152,6 +152,9 @@ import {
   SaveData,
   GetSearch,
 } from "@/api/Common";
+import {
+  HeaderCheckBoxCellType
+} from "@/static/data.js";
 import formatDates,{formatNextMonthDate,formatDate} from "@/utils/formatDate"
 import XLSX from "xlsx";
 export default {
@@ -206,6 +209,15 @@ export default {
           Methods: "Analysis",
           Icon: "",
         },
+        {
+          ButtonCode: "delete",
+          BtnName: "删除",
+          isLoading: false,
+          Methods: "deleteRow",
+          Type: "danger",
+          Icon: "",
+          Size: "small",
+        },
       ],
       // 表头添加动态按钮
       parmsBtn2:[
@@ -239,6 +251,7 @@ export default {
       spread: null, //excel初始
       fileList: [],
       file: [],
+      selectionData:[[]],
     };
   },
   activated() {
@@ -334,9 +347,11 @@ export default {
         datas.some((m, i) => {
           this.$set(this.tableColumns, i, m);
           console.log('m',m)
-          if(m.Required){
-            this.$set(this.formSearchs[i],"required",m)
-          }
+          m.forEach((n,index) => {
+            if(n.Required){
+                this.formSearchs[this.tagRemark].required.push(n)
+            }
+          });
         });
         // 获取查询的初始化字段 组件 按钮
         forms.some((x, z) => {
@@ -397,6 +412,23 @@ export default {
             size: parseInt(x.width),
           });
         });
+        // 选框
+        sheet.setCellType(
+        0,
+        0,
+        new HeaderCheckBoxCellType(),
+        GCsheets.SheetArea.colHeader
+      );
+
+      let checkbox = {
+        name: "isChecked",
+        displayName: "选择",
+        cellType: new GC.Spread.Sheets.CellTypes.CheckBox(),
+        size: 60,
+      };
+      for (var name in checkbox) {
+        colInfos[0][name] = checkbox[name];
+      }
 
         // 设置整个列头的背景色和前景色。
         /**
@@ -646,11 +678,11 @@ export default {
                 for(let x=0;x<this.formSearchs[this.tagRemark].required.length;x++){
                     if(!DataList[i][this.formSearchs[this.tagRemark].required[x]['prop']]){
                     this.$message.error(`${this.formSearchs[this.tagRemark].required[x]['label']}不能为空，请选择`)
-                    break
+                    return
                 }
                 }
                 this.adminLoading = false;
-                return
+                
         }
         }
         let res = await GetSearch(DataList, "/APSAPI/ImportDeliveryData");
@@ -726,6 +758,54 @@ export default {
         }
       }
     },
+    // 获取选中的数据
+    getSelectionData() {
+      let sheet = this.spread.getActiveSheet();
+      let newData = sheet.getDataSource();
+      this.selectionData[this.tagRemark] = [];
+      if (newData.length != 0) {
+        newData.forEach((x) => {
+          if (x.isChecked) {
+            x.ElementDeleteFlag = 1;//删除标识
+            this.selectionData[this.tagRemark].push(x);
+          }
+        });
+      }
+    },
+    deleteRow(){
+        this.getSelectionData();
+         if (this.selectionData[this.tagRemark].length == 0) {
+          this.$message.error("请选择需要删除的数据！");
+          return;
+        } else {
+          this.$confirm("删除不可恢复，确定要删除吗？", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "info",
+          })
+            .then(async () => {
+                  this.adminLoading = true;
+                  let res = await SaveData(this.selectionData[this.tagRemark]);
+                  const { result, data, count, msg } = res.data;
+                  if (result) {
+                    this.dataSearch(this.tagRemark);
+                    this.adminLoading = false;
+                    this.$message({
+                      message: msg,
+                      type: "success",
+                      dangerouslyUseHTMLString: true,
+                    });
+                  } else {
+                    this.adminLoading = false;
+                    this.$message({
+                      message: msg,
+                      type: "error",
+                      dangerouslyUseHTMLString: true,
+                    });
+                  }
+            })
+        }
+    }
   },
 };
 </script>
