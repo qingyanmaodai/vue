b4105899be1c66aad371f9fadab15030d4a3be1d
张少漫 <zhangsm@jinggon.com>
2023-02-20 22:09:57 +0800

    接口表和接口输出表

---
+++ src/views/ConfigBasic/InterfaceOutput/index.vue
@@ -0,0 +1,375 @@
+<!--接口输出-->
+<template>
+    <div
+      class="container"
+      v-loading="adminLoading"
+    >
+      <div
+        class="admin_head"
+        ref="headRef"
+      >
+        <ComSearch
+          ref="searchRef"
+          :searchData="formSearchs[0].datas"
+          :searchForm="formSearchs[0].forms"
+          :remark="0"
+          :isLoading="isLoading"
+          :btnForm="btnForm"
+          @btnClick="btnClick"
+        />
+      </div>
+      <div>
+        <div class="admin_content">
+          <div class="ant-table-title">
+            <el-row>
+              <el-col :span="4"><span class="title">{{ title }}</span></el-col>
+              <el-col
+                :span="20"
+                class="flex_flex_end"
+              >
+              </el-col>
+            </el-row>
+          </div>
+          <ComVxeTable
+            :rowKey="'RowNumber'"
+            :height="height"
+            :tableData="tableData[0]"
+            :tableHeader="tableColumns[0]"
+            :tableLoading="tableLoading[0]"
+            :remark="0"
+            :sysID="35"
+            :isEdit="isEdit"
+            :isClear="isClear[0]"
+            :pagination="tablePagination[0]"
+            @pageChange="pageChange"
+            @pageSize="pageSize"
+            @sortChange="sortChange"
+            @OutputField="OutputField"
+          />
+        </div>
+
+        <!-- 输出字段表弹框 -->
+        <DialogTable
+        title="输入参数表"
+        :tableDialog="colDialogVisible1"
+        :sysID="9033"
+        width="80%"
+        @closeDialog="colDialogVisible1 = false"
+        :searchForm="dialogSearchForm1"
+        :isToolbar="false"
+      ></DialogTable>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  var _this;
+  import ComSearch from "@/components/ComSearch";
+  import ComVxeTable from "@/components/ComVxeTable";
+  import { GetHeader, GetSearchData, ExportData, SaveData } from "@/api/Common";
+  import DialogTable from "@/components/Dialog/dialogTable";
+  export default {
+    name: "InterfaceOutput",
+    components: {
+      ComSearch,
+      ComVxeTable,
+      DialogTable
+    },
+    data() {
+      return {
+        // 输入参数表
+        dialogSearchForm1:{
+
+        },
+        // 接口输出表
+        dialogSearchForm1:{
+            
+        },
+        adminLoading: false,
+        ////////////////// Search /////////////////
+        title: this.$route.meta.title,
+        drawer: false,
+        formSearchs: [
+          {
+            datas: {},
+            forms: [],
+          },
+        ],
+        btnForm: [],
+        parmsBtn: [
+        
+        ],
+        tableData: [[]],
+        tableColumns: [[]],
+        tableLoading: [false],
+        isClear: [false],
+        tablePagination: [{ pageIndex: 1, pageSize: 50, pageTotal: 0 }],
+        height: "707px",
+        showPagination: true,
+        tagRemark: 0,
+        isLoading: false,
+        isEdit: true,
+        isUpdate: true,
+      };
+    },
+    watch: {
+      "$route.params": {
+        handler: function (val, oldVal) {
+          if (val && val.ID) {
+            this.DictionaryID = val.ID;
+            this.formSearchs[0].datas["dicID"] = 35;
+            this.$set(this.formSearchs[0].datas, "DictionaryID", val.ID);
+            this.dataSearch(0);
+          }
+        },
+        deep: true,
+        immediate: true,
+      },
+    },
+    created() {
+      _this = this;
+      this.judgeBtn();
+      this.getTableHeader();
+    },
+    mounted() {
+      setTimeout(() => {
+        this.setHeight();
+      }, 350);
+    },
+    methods: {
+      // 判断按钮权限
+      judgeBtn() {
+        let routeBtn = this.$route.meta.btns;
+        let newBtn = [];
+        let permission = false;
+        if (routeBtn.length != 0) {
+          routeBtn.forEach((x) => {
+            if (x.ButtonCode == "edit") {
+              permission = true;
+            }
+            let newData = this.parmsBtn.filter((y) => {
+              return x.ButtonCode == y.ButtonCode;
+            });
+            if (newData.length != 0) {
+              newBtn = newBtn.concat(newData);
+            }
+          });
+        }
+        this.$set(this, "btnForm", newBtn);
+        this.$set(this, "isEdit", permission);
+      },
+      // 修改自身id的字段属性
+      oneselftSysID(val) {
+        this.DictionaryID = val;
+        this.formSearchs[0].datas["dicID"] = 35;
+        this.$set(this.formSearchs[0].datas, "DictionaryID", val);
+        this.dataSearch(0);
+      },
+      // 高度控制
+      setHeight() {
+        let headHeight = this.$refs.headRef.offsetHeight;
+  
+        let rem =
+          document.documentElement.clientHeight -
+          headHeight -
+          this.$store.getters.reduceHeight;
+        let newHeight = rem + "px";
+        this.$set(this, "height", newHeight);
+      },
+      // 第几页
+      pageChange(val, remarkTb, filtertb) {
+        this.$set(this.tablePagination[remarkTb], "pageIndex", val);
+        this.getTableData(this.formSearchs[remarkTb].datas, remarkTb);
+      },
+      // 页数
+      pageSize(val, remarkTb, filtertb) {
+        this.$set(this.tablePagination[remarkTb], "pageSize", val);
+        this.getTableData(this.formSearchs[remarkTb].datas, remarkTb);
+      },
+      // 排序
+      sortChange(order, prop, remarkTb, filtertb, row, index) {
+        if (filtertb == -100) {
+          // 改变父表格的行数据
+          this.changeTableRowData(remarkTb, row, index);
+          return;
+        }
+        if (order) {
+          if (order === "desc") {
+            this.formSearchs[remarkTb].datas["sort"] = prop + " DESC";
+          } else {
+            this.formSearchs[remarkTb].datas["sort"] = prop + " ASC";
+          }
+        } else {
+          this.formSearchs[remarkTb].datas["sort"] = null;
+        }
+        this.dataSearch(remarkTb);
+      },
+      // 改变父组件表格行数据
+      changeTableRowData(remarkTb, row, index) {
+        for (let name in row) {
+          this.$set(this.tableData[remarkTb][index], name, row[name]);
+        }
+      },
+      // 统一渲染按钮事件
+      btnClick(methods, parms, index, remarkTb) {
+        if (parms) {
+          // 下标 要用的数据 标题 ref
+          this[methods](remarkTb, index, parms);
+        } else {
+          this[methods](remarkTb, index);
+        }
+      },
+      // 查询
+      dataSearch(remarkTb) {
+        this.tagRemark = remarkTb;
+        this.tableData[remarkTb] = [];
+        this.$set(this.tableLoading, remarkTb, true);
+        this.$set(this.isClear, remarkTb, true);
+        this.tablePagination[remarkTb].pageIndex = 1;
+        this.getTableData(this.formSearchs[remarkTb].datas, remarkTb);
+        setTimeout(() => {
+          this.$set(this.isClear, remarkTb, false);
+        });
+      },
+      // 重置
+      dataReset(remarkTb) {
+        for (let name in this.formSearchs[remarkTb].datas) {
+          if (name != "dicID") {
+            this.formSearchs[remarkTb].datas[name] = null;
+          }
+        }
+      },
+      // 导出
+      async dataExport(remarkTb) {
+        this.adminLoading = true;
+        let form = JSON.parse(JSON.stringify(this.formSearchs[remarkTb].datas));
+        form["rows"] = 0;
+        let res = await ExportData(form);
+        this.adminLoading = false;
+        this.$store.dispatch("user/exportData", res.data);
+      },
+      // 保存
+      async dataSave(remarkTb, index) {
+        let submitData = [];
+        if (this.isUpdate && this.tableData[remarkTb].length != 0) {
+          this.tableData[remarkTb].forEach((x) => {
+            if (x.update) {
+              submitData.push(x);
+            }
+          });
+        } else {
+          submitData = this.tableData[remarkTb];
+        }
+        this.adminLoading = true;
+        let res = await SaveData(submitData);
+        const { datas, forms, result, msg } = res.data;
+        if (result) {
+          this.$message({
+            message: msg,
+            type: "success",
+            dangerouslyUseHTMLString: true,
+          });
+          this.adminLoading = false;
+          if (this.DictionaryID) {
+            this.formSearchs[0]["DictionaryID"] = this.DictionaryID;
+          }
+          this.dataSearch(remarkTb);
+        } else {
+          this.$message({
+            message: msg,
+            type: "error",
+            dangerouslyUseHTMLString: true,
+          });
+          this.adminLoading = false;
+        }
+      },
+      // 获取表头数据
+      async getTableHeader() {
+        let IDs = [{ ID: 35 }];
+        let res = await GetHeader(IDs);
+        const { datas, forms, result, msg } = res.data;
+        if (result) {
+          // 获取每个表头
+          datas.some((m, i) => {
+            m.forEach((n) => {
+              // 进行验证
+              this.verifyDta(n);
+              if (n.childrens && n.children.length != 0) {
+                n.childrens.forEach((x) => {
+                  this.verifyDta(x);
+                });
+              }
+            });
+            this.$set(this.tableColumns, i, m);
+          });
+          // 获取查询的初始化字段 组件 按钮
+          forms.some((x, z) => {
+            this.$set(this.formSearchs[z].datas, "dicID", IDs[z].ID);
+            x.forEach((y) => {
+              if (y.prop && y.value) {
+                this.$set(this.formSearchs[z].datas, [y.prop], y.value);
+              } else {
+                this.$set(this.formSearchs[z].datas, [y.prop], "");
+              }
+            });
+            this.$set(this.formSearchs[z], "forms", x);
+          });
+          this.$set(this.formSearchs[0].datas, "DictionaryID", this.DictionaryID);
+        }
+      },
+      // 验证数据
+      verifyDta(n) {
+        for (let name in n) {
+          if (
+            (name == "component" && n[name]) ||
+            (name == "button" && n[name]) ||
+            (name == "active" && n[name])
+          ) {
+            n[name] = eval("(" + n[name] + ")");
+          }
+        }
+      },
+      // 获取表格数据
+      async getTableData(form, remarkTb) {
+        this.$set(this.tableLoading, remarkTb, true);
+        form["rows"] = this.tablePagination[remarkTb].pageSize;
+        form["page"] = this.tablePagination[remarkTb].pageIndex;
+        let newForm = JSON.parse(JSON.stringify(form));
+        if (!newForm.DictionaryID) {
+          newForm.DictionaryID = this.DictionaryID;
+          this.formSearchs[0].DictionaryID = this.DictionaryID;
+        }
+        let res = await GetSearchData(newForm);
+        const { result, data, count, msg } = res.data;
+        if (result) {
+          this.$set(this.tableData, remarkTb, data);
+          this.$set(this.tablePagination[remarkTb], "pageTotal", count);
+          this.$set(this.tableLoading, remarkTb, false);
+        } else {
+          this.$message({
+            message: msg,
+            type: "error",
+            dangerouslyUseHTMLString: true,
+          });
+          this.$set(this.tableLoading, remarkTb, false);
+        }
+      },
+      // 刷新页面
+      refrshPage() {
+        this.$store.dispatch("tagsView/delCachedView", this.$route).then(() => {
+          const { fullPath } = this.$route;
+          this.$nextTick(() => {
+            this.$router.replace({
+              path: "/redirect" + fullPath,
+            });
+          });
+        });
+      },
+      OutputField(row){
+        console.log('输出字段row',row)
+        this.colDialogVisible1 = true
+        // this.dialogSearchForm1
+      },
+    },
+  };
+  </script>
