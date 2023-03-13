237d8b1b54e82e2eb14044d61cac429651e4d53f
张少漫 <zhangsm@jinggon.com>
2023-03-13 10:53:51 +0800

    总装日计划优化

--- src/views/ScreenPrintingPlan/ScreenPrintingTodayPlan.vue
+++ src/views/ScreenPrintingPlan/ScreenPrintingTodayPlan.vue
@@ -620,6 +620,8 @@
           sheet.defaults.colWidth = 100;
           sheet.defaults.colHeaderRowHeight = 23;
           sheet.defaults.rowHeaderColWidth = 60;
+          // 重置表单
+          sheet.reset();
           let colHeader1 = [];
           let colInfos = [];
           let newData = [];
