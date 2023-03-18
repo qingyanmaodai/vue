0583769d00e7bd0d69945e335f05261c1a016846
林凯锋 <923340767@qq.com>
2023-03-18 16:50:08 +0800

    修改角色权限默认配置了菜单页面的问题

--- src/views/ConfigPermission/RoleInfo/index.vue
+++ src/views/ConfigPermission/RoleInfo/index.vue
@@ -1,13 +1,7 @@
 <!--菜单设置-->
 <template>
-  <div
-    class="container"
-    v-loading="adminLoading"
-  >
-    <div
-      class="admin_head"
-      ref="headRef"
-    >
+  <div class="container" v-loading="adminLoading">
+    <div class="admin_head" ref="headRef">
       <ComSearch
         ref="searchRef"
         :searchData="formSearchs[0].datas"
@@ -21,22 +15,17 @@
       <div class="admin_content">
         <div class="ant-table-title">
           <el-row>
-            <el-col :span="4"><span class="title">{{ title }}</span></el-col>
-            <el-col
-              :span="20"
-              class="flex_flex_end"
+            <el-col :span="4"
+              ><span class="title">{{ title }}</span></el-col
             >
-              <el-button
-                type="primary"
-                size="mini"
-                @click="openDrawer(true)"
-              >新增角色</el-button>
+            <el-col :span="20" class="flex_flex_end">
+              <el-button type="primary" size="mini" @click="openDrawer(true)"
+                >新增角色</el-button
+              >
               <el-divider direction="vertical"></el-divider>
-              <el-button
-                type="warning"
-                size="mini"
-                @click="openDrawer(false)"
-              >用户/菜单/按钮配置</el-button>
+              <el-button type="warning" size="mini" @click="openDrawer(false)"
+                >用户/菜单/按钮配置</el-button
+              >
               <!-- <el-radio-group
                 v-model="operationType"
                 size="small"
@@ -53,15 +42,12 @@
                 content="刷新"
                 placement="bottom"
               >
-                <span
-                  class="right_icon"
-                  @click="refrshPage"
-                >
+                <span class="right_icon" @click="refrshPage">
                   <span
                     role="img"
                     aria-label="redo"
                     class="anticon anticon-redo icon_size"
-                  ><svg
+                    ><svg
                       class=""
                       data-icon="redo"
                       width="1em"
@@ -71,8 +57,10 @@
                       viewBox="64 64 896 896"
                       focusable="false"
                     >
-                      <path d="M758.2 839.1C851.8 765.9 912 651.9 912 523.9 912 303 733.5 124.3 512.6 124 291.4 123.7 112 302.8 112 523.9c0 125.2 57.5 236.9 147.6 310.2 3.5 2.8 8.6 2.2 11.4-1.3l39.4-50.5c2.7-3.4 2.1-8.3-1.2-11.1-8.1-6.6-15.9-13.7-23.4-21.2a318.64 318.64 0 01-68.6-101.7C200.4 609 192 567.1 192 523.9s8.4-85.1 25.1-124.5c16.1-38.1 39.2-72.3 68.6-101.7 29.4-29.4 63.6-52.5 101.7-68.6C426.9 212.4 468.8 204 512 204s85.1 8.4 124.5 25.1c38.1 16.1 72.3 39.2 101.7 68.6 29.4 29.4 52.5 63.6 68.6 101.7 16.7 39.4 25.1 81.3 25.1 124.5s-8.4 85.1-25.1 124.5a318.64 318.64 0 01-68.6 101.7c-9.3 9.3-19.1 18-29.3 26L668.2 724a8 8 0 00-14.1 3l-39.6 162.2c-1.2 5 2.6 9.9 7.7 9.9l167 .8c6.7 0 10.5-7.7 6.3-12.9l-37.3-47.9z"></path>
-                    </svg></span>
+                      <path
+                        d="M758.2 839.1C851.8 765.9 912 651.9 912 523.9 912 303 733.5 124.3 512.6 124 291.4 123.7 112 302.8 112 523.9c0 125.2 57.5 236.9 147.6 310.2 3.5 2.8 8.6 2.2 11.4-1.3l39.4-50.5c2.7-3.4 2.1-8.3-1.2-11.1-8.1-6.6-15.9-13.7-23.4-21.2a318.64 318.64 0 01-68.6-101.7C200.4 609 192 567.1 192 523.9s8.4-85.1 25.1-124.5c16.1-38.1 39.2-72.3 68.6-101.7 29.4-29.4 63.6-52.5 101.7-68.6C426.9 212.4 468.8 204 512 204s85.1 8.4 124.5 25.1c38.1 16.1 72.3 39.2 101.7 68.6 29.4 29.4 52.5 63.6 68.6 101.7 16.7 39.4 25.1 81.3 25.1 124.5s-8.4 85.1-25.1 124.5a318.64 318.64 0 01-68.6 101.7c-9.3 9.3-19.1 18-29.3 26L668.2 724a8 8 0 00-14.1 3l-39.6 162.2c-1.2 5 2.6 9.9 7.7 9.9l167 .8c6.7 0 10.5-7.7 6.3-12.9l-37.3-47.9z"
+                      ></path></svg
+                  ></span>
                 </span>
               </el-tooltip>
               <el-divider direction="vertical"></el-divider>
@@ -100,11 +88,7 @@
       </div>
     </div>
 
-    <el-drawer
-      :title="drawerTitle"
-      :visible.sync="drawer"
-      direction="rtl"
-    >
+    <el-drawer :title="drawerTitle" :visible.sync="drawer" direction="rtl">
       <div class="drawer_body">
         <el-form
           :model="roleForm"
@@ -115,10 +99,7 @@
         >
           <el-row>
             <el-col :span="12">
-              <el-form-item
-                label="上级角色"
-                prop="ParentRoleName"
-              >
+              <el-form-item label="上级角色" prop="ParentRoleName">
                 <el-select
                   size="small"
                   ref="formRef"
@@ -143,10 +124,7 @@
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item
-                label="角色名称"
-                prop="RoleName"
-              >
+              <el-form-item label="角色名称" prop="RoleName">
                 <el-input
                   v-model="roleForm.RoleName"
                   type="text"
@@ -158,14 +136,8 @@
 
           <el-row>
             <el-col :span="12">
-              <el-form-item
-                label="状态"
-                prop="Status"
-              >
-                <el-radio-group
-                  v-model="roleForm.Status"
-                  size="small"
-                >
+              <el-form-item label="状态" prop="Status">
+                <el-radio-group v-model="roleForm.Status" size="small">
                   <el-radio :label="1">启用</el-radio>
                   <el-radio :label="0">禁用</el-radio>
                 </el-radio-group>
@@ -174,16 +146,10 @@
           </el-row>
         </el-form>
 
-        <el-form
-          label-width="90px"
-          v-show="!operationStatus"
-        >
+        <el-form label-width="90px" v-show="!operationStatus">
           <el-row>
             <el-col :span="24">
-              <el-form-item
-                label="修改类型"
-                prop=""
-              >
+              <el-form-item label="修改类型" prop="">
                 <el-radio-group
                   v-model="editType"
                   size="small"
@@ -217,14 +183,8 @@
                   </div>
                 </template>
               </el-autocomplete>
-              <i
-                class="el-icon-search iconBtn"
-                @click="searchRoleUser"
-              ></i>
-              <i
-                class="el-icon-plus iconBtn"
-                @click="addRoleUser"
-              ></i>
+              <i class="el-icon-search iconBtn" @click="searchRoleUser"></i>
+              <i class="el-icon-plus iconBtn" @click="addRoleUser"></i>
               <el-dropdown @command="handleCommand0">
                 <img src="../../../assets/svg/dot.svg" />
                 <el-dropdown-menu slot="dropdown">
@@ -233,10 +193,7 @@
               </el-dropdown>
             </div>
           </div>
-          <div
-            class="menuTree"
-            v-loading="dialogLoading[0]"
-          >
+          <div class="menuTree" v-loading="dialogLoading[0]">
             <div
               v-for="o in userData"
               :key="o.Account"
@@ -262,10 +219,9 @@
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item command="1">刷新</el-dropdown-item>
                 <!-- <el-dropdown-item command="2">取消全部</el-dropdown-item> -->
-                <el-dropdown-item
-                  command="2"
-                  divided
-                >展开全部</el-dropdown-item>
+                <el-dropdown-item command="2" divided
+                  >展开全部</el-dropdown-item
+                >
                 <el-dropdown-item command="3">折叠全部</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
@@ -281,6 +237,7 @@
               ref="roleMenuTree"
               highlight-current
               :props="menuProps"
+              :check-strictly="true"
             >
             </el-tree>
           </div>
@@ -294,10 +251,9 @@
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item command="1">刷新</el-dropdown-item>
                 <!-- <el-dropdown-item command="2">取消全部</el-dropdown-item> -->
-                <el-dropdown-item
-                  command="2"
-                  divided
-                >展开全部</el-dropdown-item>
+                <el-dropdown-item command="2" divided
+                  >展开全部</el-dropdown-item
+                >
                 <el-dropdown-item command="3">折叠全部</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
@@ -319,17 +275,14 @@
         </div>
       </div>
       <div class="drawer_footer">
-        <el-button
-          plain
-          size="small"
-          @click="drawer = false"
-        >取消</el-button>
+        <el-button plain size="small" @click="drawer = false">取消</el-button>
         <el-button
           type="primary"
           size="small"
           @click="sureAdd"
           :loading="sureLoading"
-        >确认</el-button>
+          >确认</el-button
+        >
       </div>
     </el-drawer>
   </div>
