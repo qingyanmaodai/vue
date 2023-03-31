6a7db1dd825864b84ad474976ea28a41952f660d
林凯锋 <923340767@qq.com>
2023-03-31 11:43:59 +0800

    画面新增

---
+++ src/views/PageTwoScheduling/TestVue/index.vue
@@ -0,0 +1,19 @@
+<!--待排订单管理-->
+<template>
+  <div class="container" v-loading="adminLoading"></div>
+</template>
+
+<script>
+var _this;
+export default {
+  name: "TestVue",
+  data() {
+    return {};
+  },
+  watch: {},
+  created() {},
+  activated() {},
+  mounted() {},
+  methods: {}
+};
+</script>
