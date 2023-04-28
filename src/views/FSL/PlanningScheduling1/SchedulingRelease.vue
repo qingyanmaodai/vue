1f4ef3fcad61d8eebc55d298fa5c7d7f77e5014a
allenlian952 <952845792@qq.com>
2023-04-28 16:00:58 +0800

    1楼计划接口调整

--- src/views/FSL/PlanningScheduling1/SchedulingRelease.vue
+++ src/views/FSL/PlanningScheduling1/SchedulingRelease.vue
@@ -1717,7 +1717,7 @@ export default {
         m["dicID"] = 7960;
       });
       this.adminLoading = true;
-      let res = await GetSearch(submitData, "/APSAPI/MOPlanSaveToDayPlan");
+      let res = await GetSearch(submitData, "/APSAPI/FSLMOPlanSaveToDayPlan");
       const { result, data, count, msg } = res.data;
       if (result) {
         this.dataSearch(1);
