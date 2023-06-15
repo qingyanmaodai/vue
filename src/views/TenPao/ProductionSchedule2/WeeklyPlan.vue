0509d7128943e3b615672baa8d5cea929212626f
林凯锋 <923340767@qq.com>
2023-06-15 11:31:16 +0800

    修改传参问题

--- src/views/TenPao/ProductionSchedule2/WeeklyPlan.vue
+++ src/views/TenPao/ProductionSchedule2/WeeklyPlan.vue
@@ -952,7 +952,6 @@ export default {
       }
 
       form["rows"] = this.tablePagination[remarkTb].pageSize;
-      form["Extend12"] = this.userInfo.Extend2;
       form["page"] = this.tablePagination[remarkTb].pageIndex;
       //form["ControlID"] = "205";
       let res = await GetSearchData(form);
