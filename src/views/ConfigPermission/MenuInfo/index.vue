a5deaf3871ae12a6125af81f2304925e94982eb1
林凯锋 <923340767@qq.com>
2023-07-31 17:29:53 +0800

    修改瑞能菜单的bug

--- src/views/ConfigPermission/MenuInfo/index.vue
+++ src/views/ConfigPermission/MenuInfo/index.vue
@@ -2,8 +2,15 @@
 <template>
   <div class="container" v-loading="adminLoading">
     <div class="admin_head" ref="headRef">
-      <ComSearch ref="searchRef" :searchData="formSearchs[0].datas" :searchForm="formSearchs[0].forms" :remark="0"
-        :isLoading="isLoading" :btnForm="btnForm" @btnClick="btnClick" />
+      <ComSearch
+        ref="searchRef"
+        :searchData="formSearchs[0].datas"
+        :searchForm="formSearchs[0].forms"
+        :remark="0"
+        :isLoading="isLoading"
+        :btnForm="btnForm"
+        @btnClick="btnClick"
+      />
     </div>
     <div>
       <div class="admin_content">
@@ -13,34 +20,72 @@
               <span class="title">{{ title }}</span>
             </el-col>
             <el-col :span="20" class="flex_flex_end">
-              <el-button type="primary" size="mini" @click="openDrawer(1)">新增菜单</el-button>
+              <el-button type="primary" size="mini" @click="openDrawer(1)"
+                >新增菜单</el-button
+              >
               <el-divider direction="vertical"></el-divider>
-              <el-button type="warning" size="mini" @click="openDrawer(2)">修改菜单/配置按钮</el-button>
+              <el-button type="warning" size="mini" @click="openDrawer(2)"
+                >修改菜单/配置按钮</el-button
+              >
               <el-divider direction="vertical"></el-divider>
-              <el-tooltip class="item" effect="dark" content="刷新" placement="bottom">
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="刷新"
+                placement="bottom"
+              >
                 <span class="right_icon" @click="refrshPage">
-                  <span role="img" aria-label="redo" class="anticon anticon-redo icon_size"><svg class="" data-icon="redo"
-                      width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"
-                      focusable="false">
+                  <span
+                    role="img"
+                    aria-label="redo"
+                    class="anticon anticon-redo icon_size"
+                    ><svg
+                      class=""
+                      data-icon="redo"
+                      width="1em"
+                      height="1em"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                    >
                       <path
-                        d="M758.2 839.1C851.8 765.9 912 651.9 912 523.9 912 303 733.5 124.3 512.6 124 291.4 123.7 112 302.8 112 523.9c0 125.2 57.5 236.9 147.6 310.2 3.5 2.8 8.6 2.2 11.4-1.3l39.4-50.5c2.7-3.4 2.1-8.3-1.2-11.1-8.1-6.6-15.9-13.7-23.4-21.2a318.64 318.64 0 01-68.6-101.7C200.4 609 192 567.1 192 523.9s8.4-85.1 25.1-124.5c16.1-38.1 39.2-72.3 68.6-101.7 29.4-29.4 63.6-52.5 101.7-68.6C426.9 212.4 468.8 204 512 204s85.1 8.4 124.5 25.1c38.1 16.1 72.3 39.2 101.7 68.6 29.4 29.4 52.5 63.6 68.6 101.7 16.7 39.4 25.1 81.3 25.1 124.5s-8.4 85.1-25.1 124.5a318.64 318.64 0 01-68.6 101.7c-9.3 9.3-19.1 18-29.3 26L668.2 724a8 8 0 00-14.1 3l-39.6 162.2c-1.2 5 2.6 9.9 7.7 9.9l167 .8c6.7 0 10.5-7.7 6.3-12.9l-37.3-47.9z">
-                      </path>
-                    </svg></span>
+                        d="M758.2 839.1C851.8 765.9 912 651.9 912 523.9 912 303 733.5 124.3 512.6 124 291.4 123.7 112 302.8 112 523.9c0 125.2 57.5 236.9 147.6 310.2 3.5 2.8 8.6 2.2 11.4-1.3l39.4-50.5c2.7-3.4 2.1-8.3-1.2-11.1-8.1-6.6-15.9-13.7-23.4-21.2a318.64 318.64 0 01-68.6-101.7C200.4 609 192 567.1 192 523.9s8.4-85.1 25.1-124.5c16.1-38.1 39.2-72.3 68.6-101.7 29.4-29.4 63.6-52.5 101.7-68.6C426.9 212.4 468.8 204 512 204s85.1 8.4 124.5 25.1c38.1 16.1 72.3 39.2 101.7 68.6 29.4 29.4 52.5 63.6 68.6 101.7 16.7 39.4 25.1 81.3 25.1 124.5s-8.4 85.1-25.1 124.5a318.64 318.64 0 01-68.6 101.7c-9.3 9.3-19.1 18-29.3 26L668.2 724a8 8 0 00-14.1 3l-39.6 162.2c-1.2 5 2.6 9.9 7.7 9.9l167 .8c6.7 0 10.5-7.7 6.3-12.9l-37.3-47.9z"
+                      ></path></svg
+                  ></span>
                 </span>
               </el-tooltip>
               <el-divider direction="vertical"></el-divider>
             </el-col>
           </el-row>
         </div>
-        <ComUmyTable :rowKey="'RowNumber'" :height="height" :tableData="tableData[0]" :tableHeader="tableColumns[0]"
-          :tableLoading="tableLoading[0]" :isEdit="true" :remark="0" :sysID="sysID[0].ID" :isClear="isClear[0]"
-          :pagination="tablePagination[0]" @handleRowClick="handleRowClick" @pageChange="pageChange" @pageSize="pageSize"
-          @sortChange="sortChange" />
+        <ComUmyTable
+          :rowKey="'RowNumber'"
+          :height="height"
+          :tableData="tableData[0]"
+          :tableHeader="tableColumns[0]"
+          :tableLoading="tableLoading[0]"
+          :isEdit="true"
+          :remark="0"
+          :sysID="sysID[0].ID"
+          :isClear="isClear[0]"
+          :pagination="tablePagination[0]"
+          @handleRowClick="handleRowClick"
+          @pageChange="pageChange"
+          @pageSize="pageSize"
+          @sortChange="sortChange"
+        />
       </div>
     </div>
 
-    <el-drawer :title="drawerTitle" :visible.sync="drawer" direction="rtl" :lock-scroll="false" :modal="false"
-      :size="'40%'">
+    <el-drawer
+      :title="drawerTitle"
+      :visible.sync="drawer"
+      direction="rtl"
+      :lock-scroll="false"
+      :modal="false"
+      :size="'40%'"
+    >
       <div class="drawer_body">
         <el-form label-width="90px">
           <el-row>
@@ -48,21 +93,43 @@
               <el-form-item :label="`${dialogTitle}` + '类型'" prop="">
                 <el-radio-group v-model="Type" size="small">
                   <el-radio-button label="菜单"></el-radio-button>
-                  <el-radio-button label="按钮" v-show="tagRremark == 2"></el-radio-button>
+                  <el-radio-button
+                    label="按钮"
+                    v-show="tagRremark == 2"
+                  ></el-radio-button>
                 </el-radio-group>
               </el-form-item>
             </el-col>
           </el-row>
         </el-form>
-        <el-form v-show="Type == '菜单'" :model="menuForm" :rules="menuRules" ref="menuForm" label-width="90px"
-          class="demo-ruleForm">
+        <el-form
+          v-show="Type == '菜单'"
+          :model="menuForm"
+          :rules="menuRules"
+          ref="menuForm"
+          label-width="90px"
+          class="demo-ruleForm"
+        >
           <el-row>
             <el-col :span="12">
               <el-form-item label="上级菜单" prop="ParentMenuName">
-                <el-select ref="formRef" v-model="menuForm.ParentMenuName" :clearable="true" @change="clearData">
-                  <el-option :value="menuForm.ParentCode" :label="menuForm.ParentMenuName">
-                    <el-tree ref="selectTree" :props="treeProps" :data="treeData" :expand-on-click-node="false"
-                      @node-click="selectHandleNodeClick"></el-tree>
+                <el-select
+                  ref="formRef"
+                  v-model="menuForm.ParentMenuName"
+                  :clearable="true"
+                  @change="clearData"
+                >
+                  <el-option
+                    :value="menuForm.ParentCode"
+                    :label="menuForm.ParentMenuName"
+                  >
+                    <el-tree
+                      ref="selectTree"
+                      :props="treeProps"
+                      :data="treeData"
+                      :expand-on-click-node="false"
+                      @node-click="selectHandleNodeClick"
+                    ></el-tree>
                   </el-option>
                 </el-select>
               </el-form-item>
@@ -115,13 +182,21 @@
           <el-row>
             <el-col :span="12">
               <el-form-item label="是否隐藏" prop="Hidden">
-                <el-switch v-model="menuForm.Hidden" active-color="#13ce66" inactive-color="#ff4949">
+                <el-switch
+                  v-model="menuForm.Hidden"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                >
                 </el-switch>
               </el-form-item>
             </el-col>
             <el-col :span="12">
               <el-form-item label="不缓存" prop="keepAlive">
-                <el-switch v-model="menuForm.keepAlive" active-color="#13ce66" inactive-color="#ff4949">
+                <el-switch
+                  v-model="menuForm.keepAlive"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                >
                 </el-switch>
               </el-form-item>
             </el-col>
@@ -143,10 +218,24 @@
             </el-dropdown>
           </div> -->
           <div>
-            <ComVxeTable ref="ComVxeTable" :row-key="'RowNumber'" :height="height2" :sysID="sysID[1].ID"
-              :tableData="tableData[1]" :isToolbar="false" :isClear="false" :isEdit="true" :remark="1"
-              :table-header="tableColumns[1]" :table-loading="tableLoading[1]" :pagination="tablePagination[1]"
-              @pageChange="pageChange" @pageSize="pageSize" :hasSelect="true" @sortChange="sortChange" />
+            <ComVxeTable
+              ref="ComVxeTable"
+              :row-key="'RowNumber'"
+              :height="height2"
+              :sysID="sysID[1].ID"
+              :tableData="tableData[1]"
+              :isToolbar="false"
+              :isClear="false"
+              :isEdit="true"
+              :remark="1"
+              :table-header="tableColumns[1]"
+              :table-loading="tableLoading[1]"
+              :pagination="tablePagination[1]"
+              @pageChange="pageChange"
+              @pageSize="pageSize"
+              :hasSelect="true"
+              @sortChange="sortChange"
+            />
           </div>
           <!-- <div class="btnTree">
             <el-tree
@@ -165,7 +254,13 @@
       </div>
       <div class="drawer_footer">
         <el-button plain size="small" @click="drawer = false">取消</el-button>
-        <el-button type="primary" size="small" @click="sureAdd" :loading="sureLoading">确认</el-button>
+        <el-button
+          type="primary"
+          size="small"
+          @click="sureAdd"
+          :loading="sureLoading"
+          >确认</el-button
+        >
       </div>
     </el-drawer>
   </div>
@@ -318,8 +413,8 @@ export default {
     // 删除行
     delRow(row) {
       this.$confirm("确定要删除该菜单嘛？")
-        .then((_) => { })
-        .catch((_) => { });
+        .then((_) => {})
+        .catch((_) => {});
     },
     // 单击行
     handleRowClick(row, remarkTb) {
@@ -426,7 +521,6 @@ export default {
         });
         this.getAllMenu();
         await this.dataSearch(remarkTb);
-       
       } else {
         this.$message({
           message: msg,
@@ -497,7 +591,7 @@ export default {
         .then((_) => {
           _this.dataSave(remarkTb, index, null, newData);
         })
-        .catch((_) => { });
+        .catch((_) => {});
     },
     // 通用直接保存
     async generalSaveData(newData, remarkTb, index) {
@@ -630,6 +724,7 @@ export default {
         this.drawer = true;
         this.drawerTitle = "新增菜单";
         this.dialogTitle = "新增";
+        this.Type = "菜单";
         for (var name in this.menuForm) {
           this.menuForm[name] = "";
         }
@@ -683,12 +778,12 @@ export default {
     async sureAdd() {
       // 弹框确定添加
       if (this.Type == "按钮") {
-        this.$set(this, 'sureLoading', true);
+        this.$set(this, "sureLoading", true);
         //将所选的数据和默认的数据做对比
 
         this.selectionData[1] =
           this.$refs.ComVxeTable.$refs.vxeTable.getCheckboxRecords();
-          console.log(this.selectionData[1],'this.selectionData[1]');
+        console.log(this.selectionData[1], "this.selectionData[1]");
         let sendData = this.selectionDefaultData[1]
           .map((item) => {
             const findVal = this.selectionData[1].find(
@@ -715,13 +810,13 @@ export default {
           );
         if (sendData.length === 0) {
           this.$message.error("请选择需要修改的按钮！");
-        this.$set(this, 'sureLoading', false);
+          this.$set(this, "sureLoading", false);
           return;
         }
         await this.dataSave(1, "", "", sendData);
         console.log(2);
 
-       this.sureLoading = false;
+        this.sureLoading = false;
       } else {
         this.$refs.menuForm.validate((valid) => {
           if (valid) {
